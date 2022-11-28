a0c2a947df9dec6bee557b281155505269b1d77c
Magnus Emil Gervig Svensson <mesv@itu.dk>
2022-11-28 15:41:06 +0100

    working

--- GitInsight/Repo.Entities/Usings.cs
+++ GitInsight/Repo.Entities/Usings.cs
@@ -1,13 +1,10 @@
 global using System;
-global using System.Dynamic;
-global using System.ComponentModel.DataAnnotations;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
 
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Design;
-global using Microsoft.EntityFrameworkCore.Infrastructure;
-global using Microsoft.EntityFrameworkCore.Migrations;
-global using Microsoft.EntityFrameworkCore.Storage;
-//global using Microsoft.EntityFrameworkCore;
-//global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
 
 global using Microsoft.Extensions.Configuration;
